c32e80d051c0b9765bbd197410b29b0cf6adb6b6
Kyle Brugmans <Kyle.Brugmans@gmail.com>
2019-09-19 22:34:53 -0400

    Code works up to 65535 // New text

--- numbers/src/Binary/binary.java
+++ numbers/src/Binary/binary.java
@@ -12,17 +12,22 @@ public class binary {
 	// newNumber is the constantly changing value which will store the
 	// numbers into x1 to x8 //
 	
-	static int startNumber = 8;
+	static int startNumber = 65535;
 	static int newNumber = startNumber;
 	
 	static int x1; static int x2; static int x3; static int x4; 
 	static int x5; static int x6; static int x7; static int x8;
 	static int x9; static int x10; static int x11; static int x12;
+	static int x13; static int x14; static int x15; static int x16;
 	
 	// ^^ All the data will be stored above ^^ //
 	
 	public static void InitialNumber( ) {
-		System.out.println("Decimal "+newNumber+" in binary equal to");
+		System.out.println("Pick a number anywhere between 65535 and"
+				+ " 0.\nThis program will find the binary for any of the"
+				+ "chosen decimal values.\n");
+		System.out.println("The number you choose is "+startNumber+"\n");
+		System.out.println("Decimal "+newNumber+" in binary equal to:");
 		// The initial sentence for the program...//
 	}
 	
@@ -40,13 +45,17 @@ public class binary {
 		System.out.print(x3);
 		System.out.print(x4+" "); // Spaces out but 4
 		System.out.print(x5);
-		System.out.print(x6);
+		System.out.print(x6);	
 		System.out.print(x7);
 		System.out.print(x8+" ");
 		System.out.print(x9);
 		System.out.print(x10);
 		System.out.print(x11);
-		System.out.print(x12);
+		System.out.print(x12+" ");
+		System.out.print(x13);
+		System.out.print(x14);
+		System.out.print(x15);
+		System.out.print(x16);
 		
 		// This will print the ones and zeroes in the
 		// proper order
@@ -65,17 +74,29 @@ public class binary {
 		
 		// Example using the number 9
 		
-		x12 = newNumber%2;
+		x16 = newNumber%2;
 		newNumber = newNumber/2; // 8/2 = 4 = R 0
 		
-		x11 = newNumber%2;
+		x15 = newNumber%2;
 		newNumber = newNumber/2; // 4/2 = 2 = R 0
 		
-		x10 = newNumber%2;
+		x14 = newNumber%2;
 		newNumber = newNumber/2; // 2/2 = 1 = R 0
 		
-		x9 = newNumber%2;
+		x13 = newNumber%2;
 		newNumber = newNumber/2; // 1/2 = 0.5 = R 1
+		
+		x12 = newNumber%2;
+		newNumber = newNumber/2; // 0/2 = R 0
+		
+		x11 = newNumber%2;
+		newNumber = newNumber/2; // 0/2 = R 0
+		
+		x10 = newNumber%2;
+		newNumber = newNumber/2; // 0/2 = R 0
+		
+		x9 = newNumber%2;
+		newNumber = newNumber/2; // 0/2 = R 0
 		
 		x8 = newNumber%2;
 		newNumber = newNumber/2; // 0/2 = R 0
