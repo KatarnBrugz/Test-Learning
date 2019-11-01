74c55870e5f4543e793e29b51d3ae17dc2d11f99
Kyle Brugmans <Kyle.Brugmans@gmail.com>
2019-11-01 13:52:55 -0400

    clarification

--- 8.1/src/eightOne/Sequence.java
+++ 8.1/src/eightOne/Sequence.java
@@ -8,33 +8,50 @@ public class Sequence {
 	
 	public static void initalization() {
 		
+		// First you select your sequence number
+		// Choosing how many times you want the
+		// Formula to run on for
+		
 		System.out.println("Enter an integer:");
 		
+		// Creating the scanner for input
 		Scanner declareSequence = new Scanner(System.in);
+		
+		// The sequence becomes the inputed integer
 		sequence = declareSequence.nextInt();
 		
 	}
 	
 	public static void formula() {
 		
+		// Checking whether or not the number is 0 or negative
+		// If its a negative it'll set the negative to 0
 		if (sequence <= 0) {
 			sequence = 0;
 		}
+		
+		// We set it to zero so that case 0 can be triggered properly
 		switch (sequence) {
-		case 0:
+		
+		case 0: // Was not a positive number
 			System.out.println("Put in a real number you twat");
 			break;
-		default:
+			
+		default: // Will always trigger as long as its a positive number
 		int num1 = 0; int num2 = 1; int threshhold;
+		// Declare all the numbers to add together, threshold becomes
+		// The save point
 		
 		System.out.print(num1 + " " + num2);
 		
 		for (int i = 0; i<=sequence; i++) {
 			
-			threshhold = num1 + num2;
-			num1 = num2;
-			num2 = threshhold;
+			threshhold = num1 + num2; // The save is the next INT
+			num1 = num2; // becomes the previous larger INT
+			num2 = threshhold; // becomes the the largest INT
+			
 			System.out.print(" "+ threshhold);
+			// print out all the information
 		}
 	}
 		
